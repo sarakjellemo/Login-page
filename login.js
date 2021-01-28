691b95c8a1922c0af9d9dfade9ff2ed92303758b
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-01-28 13:30:06 +0100

    login function added

--- login.js
+++ login.js
@@ -20,10 +20,54 @@ document.getElementById("createAccountBtn").addEventListener("click", function()
     //save the old data + new data to local storage
     localStorage.setItem("data", JSON.stringify(old_data));
 //}
-
 });
 
+var users = [
+    {userName: "Sara", userPassword: "Kjellemo"},
+    {userName: "Janne", userPassword: "Kemi"}
+];
+console.log("användare", users);
 
+let saveToJson = JSON.stringify(users)
+console.log("JSON", saveToJson);
+
+localStorage.setItem("users", saveToJson);
+
+let getFromStorage = localStorage.getItem("users");
+
+let getusers = JSON.parse(getFromStorage);
+console.log("Hämtad array", getusers);
+
+document.getElementById("loginBtn").addEventListener("click", function(){
+    
+    var userName = document.getElementById("usInput").value
+    var userPassword = document.getElementById("psInput").value
+    console.log("you're username is "+ userName + " and your pasword is " + userPassword);
+
+    for (i = 0; i <users.length; i++) {
+        if(userName == users[i].userName && userPassword == users[i].userPassword) {
+            console.log(userName + " is logged in!");
+            return
+        }
+    } 
+    console.log("incorrect username or password");   
+});
+
+// document.getElementById("loginBtn").addEventListener("click", function() {
+//     console.log("klick");
+//     existinglist = JSON.parse(localStorage["data"]);
+//   var uninput = document.getElementById('usInput').value;
+//   var pwinput = document.getElementById('psInput').value;
+//   for (var i=0; i <data.length; i++){
+//       if (uninput == data[i]['usInput'] && pwinput == data[i]['psInput']){
+//           Credential.alert('You have login succesfully');
+//           break;
+//       }   else {
+//           alert('Invalid login');
+//       }
+//   }
+
+// });
 
 
 // function setFormMessage(formElement, type, message){
