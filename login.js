e00b54611fcd0e9c332cb4c3241639bca20ed992
sarakjellemo <77279576+sarakjellemo@users.noreply.github.com>
2021-03-04 14:40:26 +0100

    new user can logg in and loginpage dosn't disapear after refreshing

--- login.js
+++ login.js
@@ -9,48 +9,72 @@ let users = [
 
 //CREATE NEW ACCOUNT
 const addUsers = e=>{
-    e.preventDefault();
-    let inputUsers = {
-        userName: document.getElementById("inputUserName").value,
-        userPassword: document.getElementById("inputPassword").value
-    }
+   e.preventDefault();
+   let inputUsers = {
+    userName: document.getElementById("inputUserName").value,
+    userPassword: document.getElementById("inputPassword").value
+}
     if(inputUserName.value && inputPassword.value){
-    users.push(inputUsers);
-    document.forms[0].reset();
-    document.getElementById("userAdded").innerHTML = "New account is added!"  
-    localStorage.setItem("users", JSON.stringify(users));
-    }
-    
+        users.push(inputUsers);
+        // document.forms[0].reset();
+        localStorage.setItem("users", JSON.stringify(users));
+        localStorage.setItem(`users${inputUsers}`, JSON.stringify(users));
+        document.getElementById("userAdded").innerHTML = "New account is added!"
+    } 
 }
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById("createAccountBtn").addEventListener('click', addUsers);
 });
 
+// STAY LOGGED IN
+if (localStorage.getItem("loggedInUser") != undefined) {
+     let currentUser = localStorage.getItem("loggedInUser");
+       document.getElementById("login");
+       document.getElementById("homePG")
+       login.classList.add("form-hidden");
+       homePG.classList.remove("form-hidden");
+       logOut();
+}
 //LOGIN PAGE 
-let loggedInUser = [{userName: "", userPassword: ""}];
-
+loggIn();
+function loggIn(){
 document.getElementById("loginBtn").addEventListener("click", e =>{
     var userName = document.getElementById("usInput").value
     var userPassword = document.getElementById("psInput").value
     let storedUsers = JSON.parse(localStorage.getItem("users"));
-
+    localStorage.setItem("loggedInUser", JSON.stringify (userName + userPassword));
     // console.log("you're username is "+ userName + " and your pasword is " + userPassword);
-    for (i = 0; i <storedUsers.length; i++) {
+    for (let i = 0; i <storedUsers.length; i++) {
         if(userName == storedUsers[i].userName && userPassword == storedUsers[i].userPassword) {
-          localStorage.setItem("loggedInUser", JSON.stringify (userName));
           e.preventDefault();
-
           loginForm.classList.add("form-hidden");
           homepageForm.classList.remove("form-hidden");
           document.getElementById("welcome").insertAdjacentHTML ("beforeend", userName + " is now logged in" );
           console.log(userName + " is logged in!");
-          return 
+          localStorage.setItem("loggedInUser", JSON.stringify (userName + userPassword));
+         logOut();
+         return; 
         }
     } 
     e.preventDefault();
     document.getElementById("error").innerHTML = "Incorrect username or password";
-    // console.log("incorrect username or password");   
+    // console.log("incorrect username or password"); 
 });
+}
+// LOG OUT 
+logOut();
+function logOut() {
+    let logOut = document.getElementById("loBtn");
+    logOut.addEventListener("click", function() {
+        localStorage.removeItem("loggedInUser");
+        console.log("du har loggat ut");
+        document.getElementById("login");
+        document.getElementById("homePG")
+        login.classList.remove("form-hidden");
+        homePG.classList.add("form-hidden");
+    });
+}
+
 
 //SWICH FROM LOGIN PAGE TO CREATE ACCOUNT PAGE 
 let homepageForm = document.querySelector("#homePG");
@@ -74,4 +98,4 @@ console.log("Hallå");
         createAccountForm.classList.add("form-hidden");
     });
 
-
+  
